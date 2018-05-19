b9660c244057f38216be091ab80a6403ce4870ed
famasboy88 <famasboy88@gmail.com>
2018-05-19 23:22:01 +0800

    Initial Commit

---
+++ src/main/java/com/fujitsu/jinji/JinjiApplication.java
@@ -0,0 +1,12 @@
+package com.fujitsu.jinji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinjiApplication.class, args);
+    }
+}
