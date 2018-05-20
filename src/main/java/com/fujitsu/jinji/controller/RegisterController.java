0b9927e054ff185782ecc4b81a795c71c996b866
kyleyapDev <kyleyap8888@gmail.com>
2018-05-20 20:44:33 +0800

    repository

--- src/main/java/com/fujitsu/jinji/controller/RegisterController.java
+++ src/main/java/com/fujitsu/jinji/controller/RegisterController.java
@@ -1,4 +1,27 @@
 package com.fujitsu.jinji.controller;
 
+import com.fujitsu.jinji.database.services.UserHr_Service;
+import com.fujitsu.jinji.models.UserHR_Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class RegisterController {
+    @Autowired
+    private UserHr_Service userHr_service;
+
+    @GetMapping("/register")
+    public String registerForm(Model model){
+        model.addAttribute("user_hr_model",new UserHR_Model());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("hr_model") UserHR_Model hr_model,Model model){
+        userHr_service.saveUserHR(hr_model);
+        model.addAttribute("userPending", userHr_service.findAllPending());
+        return "redirect:register";
+    }
 }
