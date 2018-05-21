05076cf97c627b797460c5b7b79794dde88eff3a
kyleyapDev <kyleyap8888@gmail.com>
2018-05-22 00:14:48 +0800

    Sec

--- src/main/java/com/fujitsu/jinji/controller/RegisterController.java
+++ src/main/java/com/fujitsu/jinji/controller/RegisterController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.MappedSuperclass;
+
 @Controller
 public class RegisterController {
     @Autowired
